8895d343d58f544ff24f837770086763297e6ba4
Zuzana Popovcová <popovcova.zuzana@gmail.com>
2020-12-14 09:40:56 +0100

    refactor

--- src/statistics/affiliations.py
+++ src/statistics/affiliations.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 def count_authors(file):
     spark = SparkSession.builder.master("local").getOrCreate()
     df = spark.read.options(header='True', delimiter='\t').csv(file)
-    df2 = spark.read.options(header='True', delimiter='\t').csv("../csv_data/results/membersOf.csv")
+    df2 = spark.read.options(header='True', delimiter='\t').csv("../data/results/membersOf.csv")
     df2 = df2.withColumnRenamed("name", "author_name")
 
     df_joined = df.join(df2, df.entity == df2.member_of, 'inner')
